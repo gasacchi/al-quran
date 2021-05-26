c7c23223f9806e4c40491323ab3dad039d99f447
Gasacchi <6asacchi@gmail.com>
2021-05-26 19:29:44 +0700

    use a instead of button

--- src/lib/shared/Button.svelte
+++ src/lib/shared/Button.svelte
@@ -1,3 +1,10 @@
-<button class={$$props.class}>
-<slot></slot>
-</button>
+<script>
+  export let href;
+</script>
+
+<a
+  {href}
+  class="font-bold focus:outline-none border border-white rounded-xl p-3 w-1/2"
+>
+  <slot />
+</a>
