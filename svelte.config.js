18445096c18bbb0a39f41fcf116ea78f8b837d49
Gasacchi <6asacchi@gmail.com>
2021-05-26 17:38:11 +0700

    init project

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,14 @@
+import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#al'
 	}
 };
 
