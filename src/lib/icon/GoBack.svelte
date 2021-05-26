70ad533ad117f606bd6c196e9f49e8614f7af502
Gasacchi <6asacchi@gmail.com>
2021-05-26 19:30:19 +0700

    add go back icon

---
+++ src/lib/icon/GoBack.svelte
@@ -0,0 +1,8 @@
+<svg class={$$props.class} viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+  ><path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M18 24L0 12 18 0v24zM1.803 12L17 22.132V1.869L1.803 12z"
+    fill="#E87A87"
+  /></svg
+>
