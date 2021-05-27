13a1f0f77ed495973953a59d2b7365f0699f8f70
Gasacchi <6asacchi@gmail.com>
2021-05-27 23:25:01 +0700

    add layout

---
+++ src/routes/daftar-surat/__layout.reset.svelte
@@ -0,0 +1,11 @@
+<script>
+  import BottomNav from "$lib/shared/BottomNav.svelte";
+  import TopNav from "$lib/shared/TopNav.svelte";
+</script>
+
+<TopNav />
+<main class="h-screen space-y-10">
+  <slot />
+</main>
+<BottomNav />
+
