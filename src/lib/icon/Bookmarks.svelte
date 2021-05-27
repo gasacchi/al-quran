ab0bd218217d6dc7b1c950cca9afc58ec1527350
Gasacchi <6asacchi@gmail.com>
2021-05-27 23:21:59 +0700

    add bookmarks icon

---
+++ src/lib/icon/Bookmarks.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let active;
+</script>
+
+<svg
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class={active
+    ? "h-12 p-2 bg-al-pink rounded-full -mt-6 fill-current text-al-pink"
+    : "h-8 fill-current text-al-blue opacity-60"}
+  ><g clip-path="url(#clip0)"
+    ><path
+      d="M20 24l-6-5.269L8 24V6.589C8 3.042 6.833 1.042 4 0h9.938A6.06 6.06 0 0120 6.042V24z"
+    /></g
+  ><defs
+    ><clipPath id="clip0"><path fill="#fff" d="M0 0h24v24H0z" /></clipPath
+    ></defs
+  ></svg
+>
