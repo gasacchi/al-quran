a9b074fb0c0bd7ab7314df31e3ca5f90b72c53fe
Gasacchi <6asacchi@gmail.com>
2021-05-27 23:22:07 +0700

    add home icon

---
+++ src/lib/icon/Home.svelte
@@ -0,0 +1,7 @@
+<svg class="h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+  ><path
+    d="M4 17.162L2 18V5.028L14 0v2.507L4 6.697v10.465zm16-8.156v8.635l-8 3.352v-8.635l8-3.352zM22 6l-12 5.028V24l12-5.028V6zM8 9.697l10-4.19V3L6 8.028V21l2-.838V9.697z"
+    fill="#154666"
+    fill-opacity=".65"
+  /></svg
+>
